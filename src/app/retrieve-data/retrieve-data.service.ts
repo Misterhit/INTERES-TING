bb671747f7d360a7e91fcaa47a252172a11f53c2
Antonio <galvanantonio.alumno@gmail.com>
2020-12-14 12:26:24 +0000

    adjust in user mode view!

--- src/app/retrieve-data/retrieve-data.service.ts
+++ src/app/retrieve-data/retrieve-data.service.ts
@@ -1,30 +1,48 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AngularFireDatabase} from '@angular/fire/database';
 import {BehaviorSubject} from 'rxjs';
-import {AuthService} from '../auth/auth.service';
 import firebase from 'firebase';
 import {User} from '../auth/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminSearchService {
+export class RetrieveDataService {
 
   adminQrCode: BehaviorSubject<string> = new BehaviorSubject<string>('');
   adminCalendar: string[] = [];
   adminObservableCalendar: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
   adminRoomNumber: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
-
-  constructor(private authService: AuthService) {
+  constructor(private fireDB: AngularFireDatabase) {
   }
 
 
-  generalSearch(roomNumber: string) {
+  getCapacity(id: string) {
+    return this.fireDB
+      .object('controlAforo/' + id)
+      .valueChanges();
+
+  }
+
+  getRoomObj(roomNumber: string) {
+    return this.fireDB
+      .object('rooms/' + roomNumber)
+      .valueChanges();
+  }
+
+  getAllRooms() {
+    return this.fireDB
+      .list('rooms/')
+      .valueChanges();
+  }
+
+  generalAdminSearch(roomNumber: string) {
     firebase.database()
       .ref('rooms/' + roomNumber).once('value')
       .then(res => {
         if (res.exists()) {
-          this.authService.getRoomObj(roomNumber).subscribe((obj: User) => {
+          this.getRoomObj(roomNumber).subscribe((obj: User) => {
             this.adminQrCode.next(obj.qrCode);
             this.adminRoomNumber.next(obj.roomNumber);
             this.adminCalendar = [];
@@ -32,9 +50,8 @@ export class AdminSearchService {
             this.adminObservableCalendar.next(this.adminCalendar);
           });
         } else {
-          alert("The room entered does not exist in our database!");
+          alert('The room entered does not exist in our database!');
         }
       });
   }
-
 }
